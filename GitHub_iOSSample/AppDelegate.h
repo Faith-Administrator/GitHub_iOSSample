3aeb6f138dc3afc3a86ec998de08fa32ca02ce4b
j_maruyama <j_maruyama@faith.co.jp>
2015-02-09 13:36:40 +0900

    Initial Commit

---
+++ GitHub_iOSSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHub_iOSSample
+//
+//  Created by j_maruyama on 2015/02/09.
+//  Copyright (c) 2015年 j_maruyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHub_iOSSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHub_iOSSample
+//
+//  Created by j_maruyama on 2015/02/09.
+//  Copyright (c) 2015年 j_maruyama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
